56a90cf526c3535d5577b4e8aa63fbc5ad15a9a5
ArturoRubin <116040655+ArturoRubin@users.noreply.github.com>
2023-03-13 14:17:14 -0600

    debugging

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -10,8 +10,8 @@ function Jumbotron() {
                     <h1 className='mb-4'>Renta Maquinaria Pesada</h1>
                     <h2 className='my-4'>¿Qué Somos?</h2>
                     <p className='my-3'>Somos el portal numero uno en mexico, para publicar
-                        y arrendar maquuiinaria implememntada en la cimentación profunda </p>
-                    <h3>Anunciate ya en nuestro portal y empieza a arrendar ya!</h3>
+                        y arrendar maquinaria pesada y ligera en México </p>
+                    <h3>Anunciate en nuestro portal y empieza a arrendar ya!</h3>
                 </div>
             </div>
         </div>
